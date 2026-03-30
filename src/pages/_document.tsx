3a03a2281d4fd2cfc658cb6b0365e77a645afea5
unknown <youngchan.louis@gmail.com>
2026-03-30 10:59:47 +0900

    fix: force light mode using color-scheme meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <meta name="color-scheme" content="light only" />
+      </Head>
       <body>
         <Main />
         <NextScript />
